1794ef69a55396b026e802af479fb8e785f7cd39
davemins <davemins4125@naver.com>
2025-07-15 13:07:42 +0900

    feat: WinForms 기본 폼 생성 및 버튼 클릭 이벤트 구현

---
+++ Step02_WinForms/Form1.cs
@@ -0,0 +1,15 @@
+namespace Step02_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("버튼이 클릭됐어요!");
+        }
+    }
+}
